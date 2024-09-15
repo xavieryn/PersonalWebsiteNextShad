4875367162347f03fc2c3e7fe7d809f82f56be0b
xavieryn <61944157+xavieryn@users.noreply.github.com>
2024-09-15 12:51:08 -0400

    Personal Website 2024 using Shadhn, Next, React

--- app/page.tsx
+++ app/page.tsx
@@ -12,16 +12,13 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        
+        <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
+            Welcome to my website!
           </li>
           <li>Save and see your changes instantly.</li>
-        </ol>
+        </ul>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
