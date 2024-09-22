29ba1244fa1dd2a800bc3d57873029bbce5c1513
xavieryn <61944157+xavieryn@users.noreply.github.com>
2024-09-22 12:43:02 -0400

    Text is within the circle

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -7,26 +7,29 @@ import { Button } from "@/components/ui/button"
 let theme = "light"
 
 
-export default function Introduction(){
+export default function Introduction() {
     const { setTheme } = useTheme()
 
-    return(
-        <div className=" flex justify-center items-center flex-col h-screen  bg-black rounded-full  border-[#243c5a] borderwidth-3">
-            <div className="text-center text-xl lg:text-4xl font-[family-name:var(--font-geist-mono)]  ">
-            
+    return (
+        <div className=" flex justify-center items-center flex-col h-screen ">
+            <div className="flex flex-col bg-white rounded-full  border-black border-4 w-3/5 h-3/5 justify-center items-center">
+                <div className="text-center text-xl lg:text-4xl font-[family-name:var(--font-geist-mono)]">
+
                     <p className="mb-2 ">
                         Welcome to my website!
                     </p>
                     <p >I'm Xavier Nishikawa.</p>
-               
-            </div>
-            <Button className="mt-5"variant="outline" size="icon" onClick={() => theme =="light" ? (setTheme("dark"), theme="dark"): (setTheme("light"), theme="light")}>
+
+                </div>
+                <Button className="mt-5" variant="outline" size="icon" onClick={() => theme == "light" ? (setTheme("dark"), theme = "dark") : (setTheme("light"), theme = "light")}>
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            </Button>
-            <SocialMedia /> 
+                </Button>
+                <SocialMedia />
+            </div>
+
         </div>
-        
+
     );
 }
 
