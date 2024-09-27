d2729f284946c77fde676a9c8527ebcc6831a977
xavieryn <61944157+xavieryn@users.noreply.github.com>
2024-09-26 23:14:09 -0400

    Finalizing About Me and Work files

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -4,9 +4,20 @@ import SocialMedia from "./SocialMedia";
 export default function AboutMe() {
     return (
         <div className="flex justify-center items-center lg:h-screen w-full mb-10"> {/* Full screen height and width, centered content */}
-            <div className="text-center sm:text-sm lg:text-xl font-[family-name:var(--font-geist-mono)] p-14">
+            <div className="text-center sm:text-sm lg:text-l font-[family-name:var(--font-geist-mono)] lg:p-14">
                 <p className="mb-2">
-                    Integer consequat ridiculus lobortis tristique nascetur, ultrices taciti? Curabitur maximus euismod varius sem ultrices montes vitae mi. Cras praesent duis volutpat phasellus massa taciti sem libero. Enim felis scelerisque eros cursus suspendisse interdum consectetur taciti nostra.
+                    Like every other programmer, I started with CS50. I quickly fell in love with the problem solving and the challenge. Over the next few years, I transitioned to Web Dev where I made a few React Apps, personal and for an internship.
+                    <br/><br/>
+                    I am now at Olin College of Engineering where I hope to further expand my knowledge of programming. Currently, I have strong interests in Machine Learning because it combines my love of programming and linear algebra and calculus. I am constantly learning, building new projects.
+                    <br/><br/>
+                    I am working on:
+                    <br/><br/>
+                    •	Andrew Ng’s ML Specialization Coursera course where I am learning the math behind the many functions used in Machine Learning.
+                    <br/>
+                    •	This personal website, refreshing my knowledge on React and Web Dev overall.
+                    <br/>
+                    •	A passion pursuit at my school where I will use an Arduino and a small DC motor to open our door without a key.
+
                 </p>
             </div>
         </div>
--- components/Work.tsx
+++ components/Work.tsx
@@ -20,25 +20,29 @@ const Work: React.FC<WorkProps> = ({ work
         <hr className="h-1 mx-auto my-4 bg-[hsl(255,67%,5%)] border-1 rounded  dark:bg-white" />
       </div>
 
-      <ul className="lg:mt-4 sm:mt-1">
+      <ul className="sm:mt-1">
         {Object.entries(work.sections).map(([key, value]) => (
-          <div className="flex flex-col bg-[hsl(40, 20%, 97%)] dark:bg-[hsl(245,23%,14%)] dark:border-white  border-black border-2 w-full mx-auto max-w-full h-3/5  rounded-2xl lg:p-5 p-2 m-2">
+          <div className="flex flex-col bg-[hsl(40, 20%, 97%)] dark:bg-[hsl(245,23%,14%)] dark:border-white  border-black border-2 w-full mx-auto max-w-full h-3/5  rounded-2xl lg:p-5 p-2 m-4">
             <li className="sm:text-xs lg:text-base" key={key}>
               <div className="flex flex-row justify-between pr-4 ">
                 <h3> {value.Company}</h3> <h3>{value.Date}</h3>
               </div>
               <h1 className="lg:text-xl font-bold p-1">{value.Title}</h1>
-              <h3>{value.Description}</h3>
+        
 
+              {value.Description ? (
+                value.Description.split('/n').map((line, index) => (
+                  
+                  <p key={index}>{line}</p>
+                ))            ) : (
+                <div>
+                  hi
+              
+                </div>
+                )}
+                
 
-              <Image
-                src={value.ImageCover}
-                width={300}
-                height={200}
-                alt="Cover Photo"
-                className="p-2 rounded-2xl" />
-
-              <div className="flex flex-wrap">
+              <div className="flex flex-wrap mt-2">
                 {value.Skills.split(',').map((skill: string, skillIndex: number) => (
                   <span key={skillIndex} className="bg-gray-200 dark:bg-[hsl(247,59,32)] px-2 py-1 rounded m-1">
                     {skill.trim()}
