b83dd5236aba140b50be56d98a24b9082a9e5172
xavieryn <61944157+xavieryn@users.noreply.github.com>
2024-09-29 22:49:54 -0400

    Switches colors, but is not responsive to mobile

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -2,29 +2,32 @@ import React, { useState, useEffect } from "react";
 import SocialMedia from "./SocialMedia";
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
+import { WiMoonFull } from "react-icons/wi";
+
 
 export default function Introduction() {
     const { theme, setTheme } = useTheme()
     const [isRotated, setIsRotated] = useState(false);
-
     useEffect(() => {
-        setTheme(theme === 'dark' ? 'dark' : 'light');
+        setTheme('light');
     }, []);
 
     const handleRotate = () => {
         setIsRotated(prev => !prev);
-        setTheme(prevTheme => prevTheme === 'dark' ? 'light' : 'dark');
+
+        if(theme == 'light'){
+            setTheme('dark')
+        } else { setTheme('light')
+        }
     };
 
     return (
         <div className="bg-center flex justify-center items-center flex-col h-screen w-full relative">
-
-
             <div className="flex flex-col bg-white bg-opacity-80 dark:bg-black dark:bg-opacity-80 rounded-full
              dark:border-white border-black border-4 w-3/5 h-3/5 lg:w-[500px] lg:h-[500px] justify-center items-center z-10">
 
                 {/* Buttons Container */}
-                <div className={`z-5 absolute  lg:w-[500px] lg:h-[500px] transition-transform duration-500 ${isRotated ? 'rotate-180' : ''}`}>
+                <div className={`z-5 absolute  lg:w-[500px] lg:h-[500px] transition-transform duration-500 dark:duration-500 ${isRotated ? 'rotate-180' : ''}`}>
                     {/* Sun Button */}
                     <button
                         onClick={handleRotate}
@@ -38,7 +41,7 @@ export default function Introduction() {
                         onClick={handleRotate}
                         className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 rounded-full p-2 bg-white dark:bg-gray-800 shadow-lg"
                     >
-                        <Moon className="h-[50px] w-[50px] text-blue-500" />
+                        <WiMoonFull className="h-[50px] w-[50px] text-blue-500" />
                     </button>
                 </div>
                 <div className="z-20 items-center justify-content flex-col flex">
